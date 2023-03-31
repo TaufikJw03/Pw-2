3d926044b8af3c63b839c1816fdbf0cbe046115d
TaufikJw03 <taufikjw3@gmail.com>
2023-03-31 23:00:06 +0700

    Praktikum Web 2 Sesi 6

---
+++ Pertemuan-6/Praktikum/kartu/delete.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../dbkoneksi.php';
+
+// Menangkap data id dari URL
+$id = $_GET['iddel'];
+
+$dbh->query("DELETE FROM kartu WHERE id=$id");
+
+header('location:index.php');
+?>
