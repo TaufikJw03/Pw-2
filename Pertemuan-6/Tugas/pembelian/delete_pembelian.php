8660023023df62a56f80f97d89d5e3c24d80d2ae
Taufik Hidayat <114472043+TaufikJw03@users.noreply.github.com>
2023-04-14 22:48:07 +0700

    Delete Pertemuan-6 directory

--- Pertemuan-6/Tugas/pembelian/delete_pembelian.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../dbkoneksi.php';
-
-// menangkap data iddel dari url
-$id = $_GET['iddel'];
-
-$dbh->query("DELETE FROM pembelian WHERE id=$id");
-
-header('location:index_pembelian.php');
--- Pertemuan-6/Tugas/vendor/delete_vendor.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../dbkoneksi.php';
-
-// menangkap data iddel dari url
-$id = $_GET['iddel'];
-
-$dbh->query("DELETE FROM vendor WHERE id=$id");
-
-header('location:index_vendor.php');
